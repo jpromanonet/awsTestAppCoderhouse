145f799f58b49c1e8a307b30d2a0c77ebb991cea
jpromanonet <programming@jpromano.net>
2022-12-10 14:54:29 -0300

    Configuración SDK AWS y parametros del CRUD

---
+++ app.js
@@ -0,0 +1,42 @@
+/* Requerir las dependencias */
+
+const express = require('express')
+const AWS = require('aws-sdk')
+const { AppStream } = require('aws-sdk')
+
+/* Configurar la región de AWS */
+AWS.config.update({
+    region: "us-east-1"
+})
+
+/* Configurar constantes de entorno de AWS */
+
+/** Configurar topico SNS **/
+const sns = new AWS.SNS()
+const SNS_TOPIC_ARN = 'arn:aws:sns:us-east-2:164802116698:notificaciones'
+
+/** Configurar DynamoDB **/
+const dynamodb = AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'product-inventory';
+
+/* Funciones del CRUD */
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("test API")
+})
+
+app.get('/api/productos', async (req, res) => {
+    const params = {
+        TableName: TABLE_NAME
+    }
+    try {
+        const productos = await scanDynamoRecords(params);
+        res.json(productos)
+    } catch (error) {
+        console.error('Ocurrio un error: ', error);
+        res.sendStatus(500);
+    }
+})
+
